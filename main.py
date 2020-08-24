e905a2f05033ff66ac4497ab112cb91d02cf4172
Anirudh <anirudh.jamkhandi@zozo.com>
2020-08-24 12:31:20 +0000

    updated github actions files

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def upload_pipeline(pipeline_name_zip: str, pipeline_name: str, kubeflow_url: st
         host=kubeflow_url,
         client_id=None,
     )
-    print ("client defined")
+    logging.info("client defined")
     client.upload_pipeline(
         pipeline_package_path=pipeline_name_zip,
         pipeline_name=pipeline_name)
